801dd6f829b8a6445d5a8a5be49b96fbf7d9b7d1
Paul Seyfert <pseyfert.mathphys@gmail.com>
2017-12-27 16:01:58 +0100

    tweak

--- format.mk
+++ format.mk
@@ -1,14 +1,18 @@
 format: format_c format_perl format_vim
 
 format_perl: urxvt/shellex.in
+	echo "[FORMAT] $^"
 	cd $(shell dirname $^) && perltidy $(shell basename $^)
 
 format_c: preload/main.c
+	echo "[FORMAT] $^"
 	clang-format -i $^
 
 format_vim:
-	for mkfile in $(wildcard *.mk) $(wildcard */*.mk) Makefile; do ./indent.sh $${mkfile} ; done
-	for shfile in $(wildcard conf/*) shellex.in ; do ./indent.sh ${{shfile}} ; done
+	echo "[FORMAT]"
+	for mkfile in $(wildcard *.mk) $(wildcard */*.mk) Makefile; do echo "[FORMAT] $${mkfile}"; ./indent.sh $${mkfile}; done
+	for shfile in $(wildcard conf/*) shellex.in; do echo "[FORMAT] $${shfile}"; ./indent.sh $${shfile}; done
+	echo "[FORMAT] .travis.yml"
 	./indent.sh .travis.yml
 
 .PHONY: format format_c format_perl format_vim
--- indent.sh
+++ indent.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 vim -f +"set softtabstop=2" +"set tabstop=2" +"set shiftwidth=2" +"set expandtab" +"gg=G" +":x" $@
+exit $?
