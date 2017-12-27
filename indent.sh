f57bc5d2aa2ab7dbb528fee1f817b33c429a6f42
Paul Seyfert <pseyfert.mathphys@gmail.com>
2017-12-27 18:58:11 +0100

    added minimalistic doc for indentation script

--- indent.sh
+++ indent.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
-vim -f +"set softtabstop=2" +"set tabstop=2" +"set shiftwidth=2" +"set expandtab" +"gg=G" +":x" $@
+
+# vim:ft=zsh
+# Helper script for CI to auto indent files
+# © 2017 Paul Seyfert and contributors (see also: LICENSE)
+
+if [ -f "$1" ]; then
+  vim -f +"set softtabstop=2" +"set tabstop=2" +"set shiftwidth=2" +"set expandtab" +"gg=G" +":x" $1
+else
+  echo "USAGE: $0 <filename>"
+  echo "to autoindent file with vim"
+fi
 exit $?
